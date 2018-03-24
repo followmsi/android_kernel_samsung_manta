30817fbed3cb0017198760a9a36dcfa0e1af83d0
followmsi <followmsi@gmail.com>
2018-03-24 19:38:22 +0100

    Revert "Always force Samsung charger unless in USB mode."
    This reverts commit aa13a6bff5f865c3f3dffdd02a704f2bf6bb6083.

--- arch/arm/mach-exynos/board-manta-battery.c
+++ arch/arm/mach-exynos/board-manta-battery.c
@@ -330,10 +330,6 @@ static enum manta_charge_source check_samsung_charger(
 			charge_source = MANTA_CHARGE_SOURCE_AC_SAMSUNG;
 	}
 
-	// Tell me again why I should care.
-	if (charge_source == MANTA_CHARGE_SOURCE_AC_OTHER)
-		charge_source = MANTA_CHARGE_SOURCE_AC_SAMSUNG;
-
 	return charge_source;
 }
 
@@ -645,8 +641,8 @@ static struct smb347_charger_platform_data smb347_chg_pdata = {
 	.pre_charge_current = 200000,
 	.termination_current = 250000,
 	.pre_to_fast_voltage = 2600000,
-	.mains_current_limit = 2500000,
-	.usb_hc_current_limit = 2500000,
+	.mains_current_limit = 2000000,
+	.usb_hc_current_limit = 1800000,
 	.irq_gpio = GPIO_TA_NCHG,
 	.disable_stat_interrupts = true,
 	.en_gpio = GPIO_TA_EN,
