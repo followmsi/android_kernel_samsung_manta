bb34cf0311d57e4cdd5a542215cc54ec850b8f7a
followmsi <followmsi@gmail.com>
2020-06-10 14:24:21 +0200

    Revert "sched: touch the hw watchdog similarly to the nmi one."
    This reverts commit fc089386ef3efbdb59bcc0197ef0106d6788e772.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -73,7 +73,6 @@
 #include <linux/slab.h>
 #include <linux/init_task.h>
 #include <linux/binfmts.h>
-#include <linux/watchdog.h>  /* for touch_hw_watchdog() */
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
@@ -5064,15 +5063,13 @@ void show_state_filter(unsigned long state_filter)
 #endif
 	rcu_read_lock();
 	do_each_thread(g, p) {
-		if (!state_filter || (p->state & state_filter)) {
-			/*
-			 * reset the NMI-timeout, listing all files on a slow
-			 * console might take a lot of time:
-			 */
-			touch_nmi_watchdog();
-			touch_hw_watchdog();
+		/*
+		 * reset the NMI-timeout, listing all files on a slow
+		 * console might take a lot of time:
+		 */
+		touch_nmi_watchdog();
+		if (!state_filter || (p->state & state_filter))
 			sched_show_task(p);
-		}
 	} while_each_thread(g, p);
 
 	touch_all_softlockup_watchdogs();
